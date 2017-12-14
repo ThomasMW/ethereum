496cc927828dc7e1572f4c1c8487c8f3bd9fdb5f
Thomas <thomasmitchellweber@gmail.com>
2017-12-14 17:30:32 +0100

    Added custom withdraw function

--- InvestCompetition.sol
+++ InvestCompetition.sol
@@ -1,51 +1,53 @@
-// This is an example contract where people can make 'investments' to a contract
-// Each investment has to be higher than the latest one to get accepted
-// Investors can attach a personal message which other people can read
-// Based on: Cryptobragging from Neel Rao @ https://cryptobragging.com/
-
 pragma solidity ^0.4.11;
 
 contract InvestCompetition {
-    // Most recent investor
+    /* Most recent investor */
     address public latestInvestor;
-
-    // Personal Message from Investor
+    
+    /* Personal Message from Investor */
     string public personalMessage;
-
-    // Highest Investment made so far
+    
+    /* Highest Investment made so far */
     uint public highestInvestment;
-
+    
     address owner;
-
+    
     function InvestCompetition() public {
         owner = msg.sender;
         highestInvestment = 0;
     }
-
+    
     function makeInvestment(string newMessage) public payable {
-        // New investment has to be higher than latest
+        /* New investment has to be higher than latest */
         require(msg.value > highestInvestment);
-
-        // Message max length
+        
+        /* Message max length */
         require(bytes(newMessage).length < 500);
-
+        
         highestInvestment = msg.value;
         latestInvestor = msg.sender;
         personalMessage = newMessage;
     }
-
+    
     function displayMessage() public constant returns(string) {
-	// Display message from latest investment
+	/* Display message from latest investment */
         return personalMessage;
     }
-
+    
     function displaytotalBalance() public constant returns(uint) {
-	// Displays the total balance of the contract
+	/* Displays the total balance of the contract */
         return this.balance;
     }
-
-    function withdrawBalance() public {
-	// Transfers the contractbalance to the owner of the contract
+    
+    function withdrawCustom(uint amount) public {
+        /* Sends a custom amount back to the owner */
+        require(msg.sender == owner);
+        owner.transfer(amount);
+    }
+    
+    function withdrawTotalBalance() public {
+	/* Transfers the total contractbalance to the owner of the contract,
+	   only the owner can do this */
         require(msg.sender == owner);
         owner.transfer(this.balance);
     }
